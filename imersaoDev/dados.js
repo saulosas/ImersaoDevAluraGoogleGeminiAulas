82de1c506c3d78b4f7ea0e777d9b90a72ea472c4
Saulo_Araujo <79018048+saulosas@users.noreply.github.com>
2024-09-05 19:40:53 -0300

    Add files via upload
    Criando uma página HTML, com CSS e Javascript. Aprendizado até a aula 04 da #ImersãoDevGoogleGemini.

---
+++ imersaoDev/dados.js
@@ -0,0 +1,13 @@
+let atletas = [{
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal: A fadinha do skate brasileiro\nRayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos 11 anos, ela já é considerada uma das maiores skatistas de todos os tempos.\n",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+},{
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade: A rainha da ginástica brasileira\nRebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas incríveis habilidades. Nas Olimpíadas de Tóquio, ela se tornou a primeira ginasta brasileira a conquistar uma medalha de ouro individual.\n",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+},{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza: A judoca brasileira que inspira gerações\nBeatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional. Com sua determinação e técnica, ela é uma das grandes promessas do judô brasileiro.\n",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+}];
